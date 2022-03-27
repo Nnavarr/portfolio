77257a10652bd9be557bcf5e3ba80b47f039cec5
eon <noe.navarro1019@gmail.com>
2022-03-27 13:52:49 -0700

    removed template code within the Portfolio component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,10 +5,10 @@ const About = () => {
     <section className='aboutme'>
       <section className='home' id='aboutme'>
         <img src={require('../../assets/images/profile_pic.jpg')} id='profile-pic' alt="Noe Navarro smiling and leaning against a wall"/>
-        <h2 id='title'>Noé Navarro | Software Developer</h2>
+        <h2 id='title' className='nameTitle'>Noé Navarro | Software Developer</h2>
       </section>
-      <h2>About Me</h2>
-      <p>
+      <h2 id='aboutme-title'>About Me</h2>
+      <p id='aboutme-paragraph'>
           Hello, I'm Noe! I graduated with a Bachelor's in Finance 2014 from the W.P. Carey School of Business. Since then, I've spent the majority of my career as a Financial Analyst at U-Haul. Through my time there, I discovered I enjoyed working with data and developing automated reports/processes much more than the finance. As a result, I decided to pursue a different career path where I'd get to do just that.  
           <br></br>
           Currently, I work for Duncan Family Farms as a Financial Systems Developer. There, I help build custom tools/processes using Python, SQL, and Power BI; aimed at improving a variety of processes from general financial month end close to reconciling various operational systems in an effort to assist both finance and ops teams. To further improve my software development skillset, during the last quarter of 2021, I enrolled into a full-stack developer bootcamp through the University of Arizona. It is through this bootcamp and self-taught resources that I hope to continue providing value to organizations by leveraging the hybrid analyst + developer skillset.
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -33,6 +33,12 @@ const Portfolio = () => {
       link: 'https://nnavarr.github.io/stockwyse/',
       image: 'stockwyse-screenshot.png',
       alt: 'Stowyse app snapshot'
+    },
+    {
+      name: 'Social Media API',
+      link: 'https://github.com/Nnavarr/social-media-api',
+      image: 'insomnia-pic.png',
+      alt: 'piture of insomnia back end'
     }
   ];
 
@@ -41,47 +47,6 @@ const Portfolio = () => {
       projectArray={projects}
     > 
     </Project>
-    // <section class='main-content'>
-    //   {/* project */}
-    //   <section class='projects' id='projects'>
-    //     <h2>Projects</h2>
-
-    //     {/* main project */}
-    //     <section class='project-second'>
-    //       <div class='banner-secondary'>
-    //         <a href='https://hyperlink-app.herokuapp.com/' target='_blank'><img src='.\assets\images\hyperlink-lobby.png' alt='hyperlink lobby'/></a> 
-    //         <div class='banner-text'>Hyperlink App</div>
-    //       </div>
-    //       <div class='col-gap'></div>
-    //       <div class='banner-secondary' id="runbuddy">
-    //           <a href='https://nnavarr.github.io/run-buddy/' target='_blank'><img src='.\assets\images\run-buddy.jpg' alt='runner kneeling down and tying their shoe'/></a> 
-    //           <div class='banner-text'>Run-Buddy Website</div>
-    //       </div>
-    //     </section>
-
-    //     {/* second row */}
-    //     <section class='project-second'>
-    //       <div class='banner-secondary'>
-    //         <a href='https://rpubs.com/nnavarr7/580588' target='_blank'><img src='.\assets\images\market-picture.jpg' alt='stock market line chart in neon style'/></a>
-    //         <div class='banner-text'>Financial Market Dashboard</div>
-    //       </div>
-    //         <div class='col-gap'></div>
-    //         <div class='banner-secondary'>
-    //           <a href='https://nnavarr.shinyapps.io/Forecast_Tool/?_ga=2.170544352.1161545448.1637389839-1798559397.1637389839' target='_blank'><img src='.\assets\images\inv-forecast.png' alt='zoom in ofa chart with trends during a business meeting'/></a>
-    //           <div class='banner-text'>Inventory Forecast</div>
-    //         </div>
-    //     </section>
-
-    //     {/* third project */}
-    //     <section class='project-second third-row'>
-    //         <div class='banner-secondary' id='stockwyse'>
-    //           <a href='https://nnavarr.github.io/stockwyse/' target='_blank'><img src='.\assets\images\stockwyse.png' alt='stockwyse app example'/></a>
-    //           <div class='banner-text'>Stockwyse App: Stock API</div>
-    //         </div>
-    //         <div class='col-gap'></div>
-    //     </section>
-    //   </section>
-    // </section>
     );
   };
 
