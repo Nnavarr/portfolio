5c4300b8e4bd8e1a9e7f79de7335138399305838
nnavarr <noe.navarro1019@gmail.com>
2023-04-02 16:44:27 -0700

    centered footer and updated color for nav items

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,12 +4,7 @@ const Footer = () => {
 
   return(
     <footer>
-      <div className='social-images'>
-        {/* github logo and link */}
-        <a href='https://github.com/Nnavarr' target='_blank' rel='noreferrer'><img src={require('../../assets/images/GitHub-Mark.png')} id='GitHub' alt='GutHub logo of octopus cat'/></a>
-        {/* linked in logo and link */}
-        <a href='https://www.linkedin.com/in/noe-navarro-27778452/' target='_blank' rel='noreferrer'><img src={require('../../assets/images/linkedin_logo.png')} id='LinkedIn' alt='LinkedIn logo of blue circle with the letters i-n'/></a>
-      </div>  
+      <p id='footer-message'>@ Noe Navarro 2023. All rights reserved</p>
     </footer>
   )
 }
