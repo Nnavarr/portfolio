d24c653564ba909b62bb51d956d35798be0870bf
nnavarr <noe.navarro1019@gmail.com>
2023-06-03 13:53:48 -0700

    updated the project container sizes and added subtitle for additional context

--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -27,25 +27,25 @@ const Resume = () => {
     {
       title: 'Data Engineer',
       subtitle: 'U-Haul International (3/2022 - Current)',
-      description: 'Intensive 6 month program covering the following: Bootstrap, CSS, Express.js, Git, Github, Heroku,  HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.'
+      description: 'As a Data Engineer, I work to design, create, implement, and monitor data pipelines related to payment processing and any Ad-Hoc pipeline requests for non-payment data. Here, I use Microsoft Azure and the Databricks environment to write Python/SQL/Spark code. One of my standout projects was to develop a Data Quality Assurance (DQA) framework to ensure data quality. Lastly, I also led the development of a React App (JavaScript) in order to visualize an existing Neo4j graph database'
     },
 
     {
       title: 'Financial Systems Developer',
       subtitle: 'Duncan Family Farms, LLC (8/2021 - 2/2022)',
-      description: 'Intensive 6 month program covering the following: Bootstrap, CSS, Express.js, Git, Github, Heroku,  HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.'
+      description: 'Assisted in the development of data aggregation processes with the goal of improving cost reporting quality across many operational and accounting systems. Here I used Python and Azure VMs to ensure our processes ran on a scheduled basis. Additionally, I developed Power BI reports, monthly financial reports, and ad-hoc financial analysis.'
     },
 
     {
       title: 'Sr. Financial / Data Analyst',
       subtitle: 'Duncan Family Farms, LLC (1/2021 - 7/2021)',
-      description: 'Intensive 6 month program covering the following: Bootstrap, CSS, Express.js, Git, Github, Heroku,  HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.'
+      description: 'Worked in a small team to conduct financial/data analysis for various operational segments of our farms. This included traditional financial analysis and spreadsheet development to analyze potential investments in farm infrastructure and overall tracking of farm performance.'
     },
 
     {
       title: 'Financial Analysis Supervisor',
       subtitle: 'U-Haul International (5/2019 - 1/2021)',
-      description: 'Intensive 6 month program covering the following: Bootstrap, CSS, Express.js, Git, Github, Heroku,  HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.'
+      description: "Managed a small team of financial analysts (3 including an intern) where I worked to implement our Director (John Good) and CFOs (Jason Berg) vision. This period also included heavy automation any/all reporting & recurring analyses using Python and SQL. See Financial Analyst below for additional info."
     },
 
     {
@@ -56,12 +56,14 @@ const Resume = () => {
       * Quarterly / Annual reporting of various business lines
       * Financial Analysis of new / existing real estate acquisitions
       * Statistical Analysis for various investment decisions
+    
+      The use of Python/SQL/R helped to automate our workflow and reporting in order to free-up time for ad-hoc analyses used in Quarterly/Annual shareholder meetings by our CFO, Jason Berg.
       `
     },
 
     {
       title: 'Lock Desk Analyst',
-      substitle: 'Movement Mortgage, LLC (6/2014 - 2/2016)',
+      subtitle: 'Movement Mortgage, LLC (6/2014 - 2/2016)',
       description: 'Worked to price and assign interest rates to mortgage applications based on buyer qualifications. This also included working directly with our Loan Officers to ensure their client met specific requirements for various state and federal loan programs.'
     }
   ]
@@ -94,9 +96,15 @@ const Resume = () => {
             {/* education */}
             {educationContainer.map(item => (
               <section className='project'>
+                {/* main title */}
                 <div className='project-title'>
                   <h3>{item.title}</h3>
                 </div>
+                {/* subtitle */}
+                <div className='project-subtitle'>
+                  <h5>{item.subtitle}</h5>
+                </div>
+
                 <p className='description-text'>{item.description}</p>
               </section>
               ))
@@ -113,6 +121,10 @@ const Resume = () => {
               <section className='project'>
                 <div className='project-title'>
                   <h3>{item.title}</h3>
+                </div>
+                {/* subtitle */}
+                <div className='project-subtitle'>
+                  <h5>{item.subtitle}</h5>
                 </div>
                 <p className='description-text'>{item.description}</p>
               </section>
