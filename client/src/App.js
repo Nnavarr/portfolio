6fece878c830f76d4b64f72c879ee83d86b142b1
nnavarr <noe.navarro1019@gmail.com>
2023-06-07 11:23:41 -0700

    beginning to update app.js structure to use routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, 
+  Route } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
@@ -8,6 +11,7 @@ import Resume from './components/Resume';
 import Footer from './components/Footer';
 import codeBackground from './assets/images/code_background.jpg';
 import rayBackground from './assets/images/ray_background.jpg';
+import { AnimatePresence } from 'framer-motion/dist/framer-motion'
 
 function App() {
 
@@ -67,27 +71,33 @@ function App() {
   }
 
   return (
-      <div className='App'>
-        <Header
-          categories={categories}
-          setCurrentCategory={setCurrentCategory}
-          handleCategoryChange={handleCategoryChange}
-          currentCategory={currentCategory}
-        ></Header>
-        <main
-          style={{ 
-            backgroundImage: `url(${backgroundImg})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center -45px',
-            backgroundAttachment: 'fixed'
-          }}
-        >
-          {/* Render selected content */}
-          {renderSelection(currentCategory)}
-        </main>
-        <Footer></Footer>
-      </div>
+    <div className='App'>
+      <AnimatePresence exitBeforeEnter >
+        <Routes>
+          <Header
+            categories={categories}
+            setCurrentCategory={setCurrentCategory}
+            handleCategoryChange={handleCategoryChange}
+            currentCategory={currentCategory}
+            ></Header>
+            <Route exact path='/' element={About}/>
+            <Route exact path='/portfolio' element={Portfolio}/>
+            <main
+              style={{ 
+                backgroundImage: `url(${backgroundImg})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center -45px',
+                backgroundAttachment: 'fixed'
+              }}
+              >
+              {/* Render selected content
+              {renderSelection(currentCategory)} */}
+          </main>
+          <Footer></Footer>
+        </Routes>
+      </AnimatePresence>
+    </div>
   );
 }
 
