a381d0e8308a41bd0c8febbb8dd4cb1cad7b7fa3
nnavarr <noe.navarro1019@gmail.com>
2023-06-20 19:22:23 -0700

    updated server.js to serve static files

--- server/server.js
+++ server/server.js
@@ -30,6 +30,8 @@ app.post('/sendEmail', async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
