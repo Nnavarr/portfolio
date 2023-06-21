7067f4f76a7ec5895a079fd93416a9c236cea890
nnavarr <noe.navarro1019@gmail.com>
2023-06-20 19:25:24 -0700

    updated code for serving static files

--- server/server.js
+++ server/server.js
@@ -30,7 +30,10 @@ app.post('/sendEmail', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
