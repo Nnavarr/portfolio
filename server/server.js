284ce21ae31fe0ee3cc2451319f32dc6f0a01a35
nnavarr <noe.navarro1019@gmail.com>
2023-06-20 20:49:17 -0700

    removed * from server

--- server/server.js
+++ server/server.js
@@ -35,10 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public'));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`The express server is listening on port ${PORT}`);
 });
